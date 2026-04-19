9286087bf424d6afb962492accb7d7bb891ecc26
= <=>
2026-04-19 12:05:23 +0300

    HOTFIX

--- index.php
+++ index.php
@@ -482,7 +482,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Симплекс-метод:</title>
+    <title>Симплекс-метод</title>
     <style>
         body {
             font-family: Arial, sans-serif;
